ded615eddc46058efcd11a32df7b22d046d6e9da
Kevin <gaohongwei@gmail.com>
2014-08-20 17:55:47 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,9 @@
 touch README.md
 git init
 git add README.md
+git reset HEAD file1 # unstage
+git checkout f1 #discard changes
 git commit -m "first commit"
+git remote -v
 git remote add origin https://github.com/gaohongwei/shell_script.git
 git push -u origin master
