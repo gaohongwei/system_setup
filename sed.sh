91ef88b61b96ad6a4edbb1754443608163bf2c5a
Kevin <gaohongwei@gmail.com>
2014-09-01 16:16:54 -0700

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -4,3 +4,6 @@ sed -i "2iresources  :users" $fname
 
 Delete line
 sed -i -e "/devise_for/d" $fname
+
+Add a line before the end of file
+sed -i '$i get "/*url" => "application/pages#show", as: :page' $fname
