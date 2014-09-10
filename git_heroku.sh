232d055772ed30ca96d1b9161664d68257e19d73
Kevin <gaohongwei@gmail.com>
2014-09-10 10:33:49 -0700

    Update git_heroku.sh

--- git_heroku.sh
+++ git_heroku.sh
@@ -42,3 +42,38 @@ Upgrade Git version >= 1.7.10 on CentOS 6
 http://tecadmin.net/how-to-upgrade-git-version-1-7-10-on-centos-6/
 
 git clone https://username:password@github.com/username/repository.git
+
+#######################################
+Install heroku
+gem install heroku
+heroku --version
+
+
+git init
+git add .
+git commit -m "add root routes"
+git remote -v
+gem install heroku
+heroku git:remote -a iask
+heroku keys:add
+git push heroku master --force
+heroku run rake db:migrate
+heroku run rake db:seed
+heroku restart
+
+heroku logs
+heroku ps:scale web=1
+heroku pg:reset TCM_productionheroku restart
+
+
+git reset --soft HEAD~3
+git rm -rf directory
+git add directoryname
+
+clone code from heroku
+heroku git:clone -a myapp
+
+Permission denied (publickey).
+heroku keys:add ~/.ssh/id_rsa.pub
+heroku git:clone -a myapp
+
