3fd6baceeac2124d9553244837655bd1b243e698
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-02-11 10:05:07 -0800

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,3 +1,7 @@
 
 #forward port 80 to 3000 ubuntu
 #sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+
+[root@stghttpd001 ~]# telnet github.com 443
+Trying 192.30.252.130...
+telnet: connect to address 192.30.252.130: Connection refused
