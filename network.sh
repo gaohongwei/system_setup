fd8d11d457c2da81f2bb56aca4f4282d1dc68a2e
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-03-03 13:28:44 -0800

    Update network.sh

--- network.sh
+++ network.sh
@@ -5,3 +5,8 @@
 [root@stghttpd001 ~]# telnet github.com 443
 Trying 192.30.252.130...
 telnet: connect to address 192.30.252.130: Connection refused
+
+
+#Forward port 80 to 3000
+# Rub it on rails server
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
