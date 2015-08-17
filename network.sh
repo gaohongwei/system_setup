dde86801bbf93cf614c4169f6979313ea09aade7
kevin-wei-pan <kwei@paloaltonetworks.com>
2015-08-16 20:01:29 -0700

    Update network.sh

--- network.sh
+++ network.sh
@@ -10,3 +10,8 @@ telnet: connect to address 192.30.252.130: Connection refused
 #Forward port 80 to 3000
 # Rub it on rails server
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+
+login=kwei@172.16.16.136
+#ssh-keygen -t rsa
+#ssh-copy-id $login
+cat ~/.ssh/id_rsa.pub | ssh $login 'mkdir ~/.ssh;cat >> ~/.ssh/authorized_keys;chmod 700 ~/.ssh; chmod 640 ~/.ssh/authorized_keys'
