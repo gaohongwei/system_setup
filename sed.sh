aec778372d7f9abbd681e06196dd31152e4538bc
kevin-wei-pan <kwei@paloaltonetworks.com>
2015-08-30 14:04:58 -0700

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -13,3 +13,7 @@ sed -e "s/.*node.name.*/node.name:154/g" $fname
 
 Replace with different strings
 sed -i 's/foo/bar/g; s/baz/zab/g; s/Alice/Joan/g' $fname
+
+Multiple repalce and pattern
+sed  -e "s|[ \t]*\([a-zA-Z0-9]*\).*integer.*|\1:integer|g" \
+     -e "s|[ \t]*\([a-zA-Z0-9]*\).*varchar.*|\1:string|g"  b.txt
