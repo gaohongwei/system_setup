8985c61e85e6a7ff61cd59aeeceb51e8c4f9c3db
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-12 00:11:18 -0800

    Update kafka_node.sh

--- kafka_node.sh
+++ kafka_node.sh
@@ -10,3 +10,11 @@ npm install kafka-node
 useradd kafka -m
 #passwd kafka
 adduser kafka sudo # add to sudo group
+
+ver=0.10.1.0
+www="http://mirror.cc.columbia.edu/pub/software/apache/kafka/$ver/kafka_2.11-$ver.tgz"
+wget $www -O kafka.tgz
+
+mkdir kafka
+tar -vxf kafka.tgz -C ./kafka --strip-components=1
+tar -xvf kafka.tgz -C ./kafka --strip 1
