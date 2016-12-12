68d3835c1087d809a6d0dfdcc3ecfc845cdc8d96
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-12-11 21:32:41 -0800

    Create kafka_node.sh

---
+++ kafka_node.sh
@@ -0,0 +1,7 @@
+pkgs="default-jre zookeeperd nodejs npm"
+for pkg in $pkgs;do
+  dpkg -s $pkg 2>/dev/null >/dev/null || apt-get install $pkg -y
+
+done
+#apt-get install $pkgs -y
+npm install kafka-node
