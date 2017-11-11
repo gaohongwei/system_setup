8f227c1b4401282eec6e321fbf9651a54e786e7a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-11-11 11:44:35 -0800

    Update find.sh

--- shell/find.sh
+++ shell/find.sh
@@ -1,6 +1,24 @@
 # Find file with contents
+find spec -name *.*|xargs grep "pattern"
+#
 grep -rnw spec -e "pattern"
 grep -rnw spec -e "ResourceHelpers::PARTNERS"
 
-# 
-find spec -name *.*|xargs grep "pattern"
+grep -rl -e before_action .
+grep -rL -e before_action .
+grep -rn -e before_action . 
+grep -rn    before_action .
+
+   -i, --ignore-case
+   -R, --recursive
+   -r, --recursive   
+   -l, --files-with-matches   
+   -L, --files-without-match
+   -n, --line-number
+   -w
+   --word-regexp
+       Select only those lines containing matches that form whole words.
+       The test is that the matching substring must either be at the beginning of
+       the line, or preceded by a non-word constituent character. Similarly, it
+       must be either at the end of the line or followed by a non-word constituent
+       character. Word-constituent characters are letters, digits, and the underscore.
