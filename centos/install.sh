e00895a4a26ba0ef5f911bd0604b974471f18cd1
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-11-17 15:40:52 -0800

    Update install.sh

--- centos/install.sh
+++ centos/install.sh
@@ -4,6 +4,7 @@ centos-release-7-4.1708.el7.centos.x86_64
 sudo yum update
 sudo yum install curl
 sudo yum install git
+sudo yum install file-devel # libmagic
 
 curl --help
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
