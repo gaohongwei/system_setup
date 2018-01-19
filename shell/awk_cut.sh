123d7ee03a2c2c3147751dcbfc5fcca9e918a14b
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-01-19 08:40:19 -0800

    Update awk_cut.sh

--- shell/awk_cut.sh
+++ shell/awk_cut.sh
@@ -2,4 +2,13 @@ Cut samples
 
 cut -d':' -f2 a.out
 awk '{print $2}'
-awk '{print $NF2}'
+awk '{print $NF}'
+awk '{print $(NF-1)}'
+
+awk -F: '{ print $1 }'     /etc/passwd
+awk -F: '{ print $1,$3 }'  /etc/passwd
+awk -F: '{ print $1,$NF }' /etc/passwd
+awk -F: '{ print $1, $(NF-1) }' /etc/passwd
+
+$(NF-2) seems not work
+For more coulmns, a loop can be used inside the expression
