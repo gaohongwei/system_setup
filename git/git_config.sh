34c95a162a7d8a456e04312e228fe4ccd51b7af6
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-01-20 10:11:42 -0800

    Update git_config.sh

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,3 +1,46 @@
+######## install git ########
+  sudo apt-get install git
+######## Create & Add ssh pub key to git ########
+  ssh-keygen -t rsa -C "kwei@gmail.com"
+  copy key and add to git
+  /home/web/.ssh/id_rsa.pub
+
+######## Testing your SSH connection ########
+  ssh -T git@github.com
+
+######## Set user and email globally ########
+  git init
+  git config --global user.name "Gaohong Wei"
+  git config --global user.name
+  git config --global user.email "xxx@gmail.com"
+  git config --global user.email
+
+  # Set user and email for single  project
+  git init
+  git config user.name "Gaohong Wei"
+  git config user.name
+  git config user.email "xxx@gmail.com"
+  git config user.email
+
+######## Check & Set remote ########
+  git remote -v
+  Get the new url from git repo
+  git remote set-url origin git@github.com:gaohongwei/ror.git
+  git remote -v
+
+######## .git/config ########
+######## Modify the ssh config ########
+#eval "$(ssh-agent -s)"
+$ cat ~/.ssh/config
+Host github.com
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_rsa1
+
+Host github.com
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_rsa2
 ######## Use 2 accounts ########
 # https://gist.github.com/jexchan/2351996
 # https://confluence.atlassian.com/bitbucket/configure-multiple-ssh-identities-for-gitbash-mac-osx-linux-271943168.html
@@ -15,36 +58,3 @@ $ ssh-add ~/.ssh/id_rsa2
 ######## Check saved keys ########
 $ ssh-add -D
 $ ssh-add -l
-
-######## Modify the ssh config ########
-#eval "$(ssh-agent -s)"
-$ cat ~/.ssh/config
-Host github.com
-  HostName github.com
-  User git
-  IdentityFile ~/.ssh/id_rsa1
-
-Host github.com
-  HostName github.com
-  User git
-  IdentityFile ~/.ssh/id_rsa2
-  
-######## Add pub key to git ######## 
-
-######## Testing your SSH connection ########
-ssh -T git@github.com
-
-######## Set user and email globally ########
-git init
-git config --global user.name "Gaohong Wei"
-git config --global user.name
-git config --global user.email "xxx@gmail.com"
-git config --global user.email
-
-# Set user and email for single  project
-git init
-git config user.name "Gaohong Wei"
-git config user.name
-git config user.email "xxx@gmail.com"
-git config user.email
-######## .git/config ########
