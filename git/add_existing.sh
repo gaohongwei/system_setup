389b8b935dc2b190d99adac6de67b32cd847840c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-03-30 20:24:31 -0700

    Create add_existing.sh

---
+++ git/add_existing.sh
@@ -0,0 +1,15 @@
+https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+
+repo='https://abc' #
+repo='git@' # ssh
+git init
+git add .
+git commit -m "First commit"
+
+Add repo
+  git remote add origin $repo
+Or reset repo
+  git remote set-url origin $repo
+
+git remote -v
+git push -u origin master
