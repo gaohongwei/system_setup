c13349fd80143b1bd88fc7967099b74b25b5668b
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-02 15:57:37 -0700

    Update rails.sh

--- vagrant/rails.sh
+++ vagrant/rails.sh
@@ -1,27 +1,27 @@
 Download and Install vagrant/virtualbox
 http://www.virtualbox.org/wiki/Downloads
 
-vagrant list
-# Create Vagrantfile
-vagrant init hashicorp/precise64
-
-
-vagrant box add bento/ubuntu-16.04
-vagrant box add bento/ubuntu-17.10
-vagrant box add hashicorp/precise64
-vagrant box add ubuntu/trusty64
-vagrant up
-vagrant list
-vagrant box remove -f hashicorp/precise64
-vagrant box remove -f ubuntu/trusty64
-
-Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/trusty64"
-  #onfig.vm.box = "bento/centos-7.4"
-  config.vm.network :private_network, ip: "192.168.10.10"
-  config.vm.network :forwarded_port, guest: 3000, host: 3000, host_ip: "localhost"
-  config.vm.synced_folder "/Users/kevin.wei/aprivate", "/home/vagrant/aprivate"
+Vagrant.configure("2") do |config|  
+  config.vm.box = "bento/ubuntu-18.04"  
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 2048
+  end
+  #config.ssh.username = "web"
+  #onfig.ssh.username = "password"
+  #onfig.vm.network :private_network, ip: "192.168.10.10"
+  #onfig.vm.network "forwarded_port", guest: 5000, host: 5000, host_ip: "localhost", protocol: "tcp", auto_correct: true
+  config.vm.network "forwarded_port", guest: 9000, host: 9000, auto_correct: true
+  config.vm.network "forwarded_port", guest: 22,   host: 2222, auto_correct: true
+     
+  config.vm.synced_folder "/Users/kwei/code", "/home/web/code"
+  config.vm.synced_folder "/Users/kwei/atry", "/home/web/atry"  
 end
+
+#ssh-copy-id -p 2222 web@localhost
+#ssh -p 2222 web@localhost
+#ssh -p '2222' 'web@localhost'
+# vagrant box list
+# vagrant install ubuntu/trusty64 
 
 Start rails s by
 rails s -b 0.0.0.0
