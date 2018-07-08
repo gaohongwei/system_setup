8e6c1a24738fbd40409dc6f493aebf2a096d4be7
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-07-08 10:57:14 -0700

    Update note.sh

--- service/note.sh
+++ service/note.sh
@@ -1,3 +1,17 @@
 
 sudo update-rc.d apache2 disable
 sudo update-rc.d apache2 enable
+
+
+Upstart systemd Supervisord
+
+Upstart
+Used as the default init replacement on Ubuntu, Upstart is very good at starting things up. 
+
+systemd
+used as the default init on a fair number of Linuxes, most notably the Redhat/Fedora family of distributions.
+
+Supervisord 
+convenient to set up, and seems to get a fair amount of use in Pythonland. 
+Its main advantage over Upstart, Runit, and systemd is that Supervisord can easily start pools of several instances of a program. 
+This is useful if you want a pool of worker processes, e.g. as FastCGI workers. 
