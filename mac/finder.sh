8cb5b5fba3aa7da1df1b8c59fbf472824c6ca5d3
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-11-02 15:28:09 -0700

    Update finder.sh

--- mac/finder.sh
+++ mac/finder.sh
@@ -1,2 +1,5 @@
 Show Full path(command)
+  On:
   defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES;killall Finder
+  Off:
+  defaults write com.apple.finder _FXShowPosixPathInTitle -bool NO;killall Finder
